282009ca3d3df3cd12cb4e5215f35d5043014876
Rishi-Puri-Goswami <rishigithub9@gmail.com>
2025-09-25 22:19:27 +0530

    ok

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://vectoraichat.netlify.app',
     credentials: true
 }))
 app.use(express.json());
